76cc3f89345e72b7b706d3491e7bd2227c4a8188
Jonathan Gzz <jonathangzzben@gmail.com>
2021-02-15 19:21:56 -0600

    Modify views

--- FreeMoney.Web/Pages/Index.cshtml.cs
+++ FreeMoney.Web/Pages/Index.cshtml.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
 
 namespace FreeMoney.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        [Required(ErrorMessage = "The name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +29,12 @@ namespace FreeMoney.Web.Pages
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost([FromForm] string name)
+        {
+            Console.WriteLine(name);
+            return RedirectToPage("Index");
         }
     }
 }
